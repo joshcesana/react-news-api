01d93470ce790538e449cafd7f53242bf295ca50
JCesana <joshuacesana@gmail.com>
2018-12-27 13:52:39 -0800

    import BrowserRouter

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
-
+import { BrowserRouter } from 'react-router-dom';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <h1>news api</h1>
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <h1>News api</h1>
+        </div>
+      </BrowserRouter>
     );
   }
 }
