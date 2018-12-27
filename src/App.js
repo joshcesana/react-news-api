b7588e87f8252c4ae4b75adb9a35cb101c8a93a8
JCesana <joshuacesana@gmail.com>
2018-12-27 13:58:53 -0800

    import/add Navbar component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 import { BrowserRouter } from 'react-router-dom';
+import Navbar from './components/Navbar.js';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <div className="App">
-          <h1>News api</h1>
+          <Navbar />
         </div>
       </BrowserRouter>
     );
