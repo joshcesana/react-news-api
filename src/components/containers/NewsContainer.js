ce0e4ef84d1354a46b639bb1886b8e12642f0686
JCesana <joshuacesana@gmail.com>
2018-12-27 14:20:23 -0800

    create containers

---
+++ src/components/containers/NewsContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class NewsContainer extends Component {
+  render() {
+    return (
+      <div>
+        <h1>NewsContainer</h1>
+      </div>
+    )
+  }
+}
+
+export default NewsContainer;
---
+++ src/components/containers/SavesContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SavesContainer extends Component {
+  render() {
+    return (
+      <div>
+        <h1>SavesContainer</h1>
+      </div>
+    )
+  }
+}
+
+export default SavesContainer;
