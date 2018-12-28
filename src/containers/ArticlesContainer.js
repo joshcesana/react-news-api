e27f293849e7bcf8ae3fb961b77382f1ee266dcd
JCesana <joshuacesana@gmail.com>
2018-12-27 22:08:28 -0800

    rename NewsContainer to ArticlesContainer

---
+++ src/containers/ArticlesContainer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Articles from '../components/Articles';
+
+class ArticlesContainer extends Component {
+  render() {
+    return (
+      <div>
+        <h1>ArticlesContainer</h1>
+        <Articles />
+      </div>
+    )
+  }
+}
+
+export default ArticlesContainer;
