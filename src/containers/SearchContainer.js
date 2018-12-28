eef1afb6c12e3aa61c69271240cab17433d6b8be
JCesana <joshuacesana@gmail.com>
2018-12-27 22:09:32 -0800

    add axios and react-redux

--- src/containers/SearchContainer.js
+++ src/containers/SearchContainer.js
@@ -1,6 +1,29 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { connect } from 'react-redux';
 
 class SearchContainer extends Component {
+
+  state = {
+    articles: []
+  }
+
+  componentDidMount() {
+    // axios.get(process.env.REACT_APP_API_URL, {
+    //   params: {
+    //     country: process.env.REACT_APP_API_COUNTRY,
+    //     category: process.env.REACT_APP_API_CATEGORY,
+    //     apiKey: process.env.REACT_APP_API_KEY
+    //   }
+    // })
+    // .then(response => {
+    //   console.log('response:', response);
+    // })
+    // .catch(error => {
+    //   console.log('API error:', error);
+    // })
+  }
+
   render() {
     return (
       <div>
@@ -10,4 +33,4 @@ class SearchContainer extends Component {
   }
 }
 
-export default SearchContainer;
+export default connect()(SearchContainer);
