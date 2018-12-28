d2d32408d82920ea813b50ec17ed0989456e2c87
JCesana <joshuacesana@gmail.com>
2018-12-27 22:10:54 -0800

    init

---
+++ src/components/Articles.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Articles extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Articles</h1>
+      </div>
+    )
+  }
+}
+
+export default Articles;
