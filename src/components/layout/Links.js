de75c861b2b9af9d62268dd965e40c001169ddd4
JCesana <joshuacesana@gmail.com>
2018-12-27 18:32:31 -0800

    change links, add icons

--- src/components/layout/Links.js
+++ src/components/layout/Links.js
@@ -4,10 +4,16 @@ import { NavLink } from 'react-router-dom';
 const Links = () => {
   return (
     <ul className="right">
-      <li><NavLink to='/'>News</NavLink></li>
-      <li><NavLink to='/'>Tech</NavLink></li>
-      <li><NavLink to='/'>Ent</NavLink></li>
-      <li><NavLink to='/'>Faves</NavLink></li>
+      <li>
+        <NavLink to='/'>
+          <i class="material-icons">search</i>
+        </NavLink>
+      </li>
+      <li>
+        <NavLink to='/'>
+          <i class="material-icons">favorite_border</i>
+        </NavLink>
+      </li>
     </ul>
   )
 }
