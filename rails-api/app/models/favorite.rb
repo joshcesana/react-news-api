8b5a631ea3b975fdc34c8997790e4ca70424ea68
JCesana <joshuacesana@gmail.com>
2018-12-27 22:14:39 -0800

    remove rails-api

--- rails-api/app/models/favorite.rb
+++
@@ -1,3 +0,0 @@
-class Favorite < ApplicationRecord
-  belongs_to :article
-end
--- rails-api/db/migrate/20181228060344_create_favorites.rb
+++
@@ -1,9 +0,0 @@
-class CreateFavorites < ActiveRecord::Migration[5.2]
-  def change
-    create_table :favorites do |t|
-      t.references :article, foreign_key: true
-
-      t.timestamps
-    end
-  end
-end
