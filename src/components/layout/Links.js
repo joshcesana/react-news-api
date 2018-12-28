2ac8914100dfea1f7831ccf3d427226e6daf7251
JCesana <joshuacesana@gmail.com>
2018-12-27 22:07:47 -0800

    change class to className

--- src/components/layout/Links.js
+++ src/components/layout/Links.js
@@ -6,12 +6,12 @@ const Links = () => {
     <ul className="right">
       <li>
         <NavLink to='/'>
-          <i class="material-icons">search</i>
+          <i className="material-icons">search</i>
         </NavLink>
       </li>
       <li>
         <NavLink to='/'>
-          <i class="material-icons">favorite_border</i>
+          <i className="material-icons">favorite_border</i>
         </NavLink>
       </li>
     </ul>
