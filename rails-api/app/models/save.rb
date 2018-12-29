166fcf97fb81835561d918a0ed6a382ee68560e9
JCesana <joshuacesana@gmail.com>
2018-12-29 15:21:05 -0800

    add validations: all columns must be present

--- rails-api/app/models/save.rb
+++ rails-api/app/models/save.rb
@@ -1,2 +1,11 @@
 class Save < ApplicationRecord
+  validates :article_id,
+            :source,
+            :author,
+            :title,
+            :description,
+            :url,
+            :image_url,
+            :publish_date,
+            presence: true
 end
