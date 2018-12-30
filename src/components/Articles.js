a290f757529e4fa01d4dd7dbe0349a23afc15856
JCesana <joshuacesana@gmail.com>
2018-12-30 13:55:40 -0800

    Delete renderArticles(), use rows with ArticleRow

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -1,33 +1,24 @@
-import React, { Component } from 'react';
-import dateformat from 'dateformat';
+import React from 'react';
+import ArticleRow from './ArticleRow';
+
 
 const Articles = (props) => {
 
-  console.log('Articles props', props);
+  const rows = props.articles.reduce((resultArray, item, index) => {
+    const chunkIndex = Math.floor(index/3) // 3 articles per row
 
-  let renderArticles = () =>
-    props.articles.map(article =>
-      <div className="row">
-        <div className="col s12 l6">
-          <div className="card" key={article.title}>
-            <div className="card-image">
-              <a href={article.url} target="_blank"><img src={article.urlToImage} alt="" /></a>
-            </div>
-            <div className="card-content">
-              <p><strong>{article.title}</strong></p>
-              <p>By: {article.author}</p>
-              <p>{dateformat(article.publishedAt, "mmmm d, yyyy h:MM:ss TT")}</p>
-              <p>{article.description}</p>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
+    if(!resultArray[chunkIndex]) {
+      resultArray[chunkIndex] = [] // start a new row
+    }
 
+    resultArray[chunkIndex].push(item)
+
+    return resultArray
+  }, [])
 
   return (
     <div>
-      {renderArticles()}
+      {rows.map((row, i) => <ArticleRow key={i} row={row} />)}
     </div>
   )
 }
