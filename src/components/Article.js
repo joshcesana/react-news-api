34de18202d282b923dc32a9e1691a45a33a55bcd
JCesana <joshuacesana@gmail.com>
2018-12-30 15:42:21 -0800

    Refactor - break up in to smaller components

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,34 +1,39 @@
 import React from 'react';
 import dateformat from 'dateformat';
 
-const Article = (props) => {
-
-  const renderArticle = () => {
-    if (props.article) {
-      return (
-        <div className="card">
-          <div className="card-image">
-            <a href={props.article.url} target="_blank"><img src={props.article.urlToImage} alt="" /></a>
-          </div>
-          <div className="card-content">
-            <p><strong>{props.article.title}</strong></p>
-            <p>By: {props.article.author}</p>
-            <p>{dateformat(props.article.publishedAt, "mmmm d, yyyy h:MM:ss TT")}</p>
-            <p>{props.article.description}</p>
-          </div>
-        </div>
-      )
-    } else {
-      return null
-    }
+const Article = props => {
+  if (props.article) {
+    return (
+      <div className="card">
+        <CardImage article={props.article} />
+        <CardContent article={props.article} />
+      </div>
+    )
+  } else {
+    return null
   }
+}
 
+const CardImage = props => {
   return (
-    <div>
-      {renderArticle()}
+    <div className="card-image">
+      <a href={props.article.url} target="_blank"><img src={props.article.urlToImage} alt="" /></a>
+      <a className="btn-floating halfway-fab waves-effect waves-light white"><i className="material-icons md-dark">favorite_border</i></a>
     </div>
-
   )
+
+}
+
+const CardContent = props => {
+  return (
+    <div className="card-content">
+      <p><strong>{props.article.title}</strong></p>
+      <p>By: {props.article.author}</p>
+      <p>{dateformat(props.article.publishedAt, "mmmm d, yyyy h:MM:ss TT")}</p>
+      <p>{props.article.description}</p>
+    </div>
+  )
+
 }
 
 export default Article;
