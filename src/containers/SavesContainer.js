3b958bcc97b455ca75cf51202c86c2f15f93ecd3
JCesana <joshuacesana@gmail.com>
2018-12-30 17:27:09 -0800

    Add redux

--- src/containers/SavesContainer.js
+++ src/containers/SavesContainer.js
@@ -1,13 +1,23 @@
 import React, { Component } from 'react';
+import ArticleList from '../components/ArticleList';
+import { connect } from 'react-redux';
 
 class SavesContainer extends Component {
   render() {
+    console.log('SavesContainer redux props:', this.props);
     return (
       <div>
-        <h1>SavesContainer</h1>
+        <h1>Saves</h1>
+        <ArticleList articles={this.props.articles}/>
       </div>
     )
   }
 }
 
-export default SavesContainer;
+const mapStateToProps = (state) => {
+  return {
+    articles: state.articles
+  }
+}
+
+export default connect(mapStateToProps)(SavesContainer);
