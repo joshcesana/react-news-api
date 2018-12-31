c6f048616f1f8358eaa10e0b69852e0747284414
JCesana <joshuacesana@gmail.com>
2018-12-30 23:49:07 -0800

    Init SearchForm component

---
+++ src/components/SearchForm.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SearchForm extends Component {
+  render() {
+    return (
+      <p>SearchForm</p>
+    )
+  }
+
+
+}
+
+export default SearchForm;
