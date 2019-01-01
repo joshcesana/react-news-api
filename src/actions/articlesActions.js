259a32a605aba0d77ae34018eaadf05086197cfe
JCesana <joshuacesana@gmail.com>
2018-12-31 19:06:28 -0800

    Use dummy data for now to reduce API calls

--- src/actions/articlesActions.js
+++ src/actions/articlesActions.js
@@ -1,22 +1,28 @@
 import axios from 'axios';
+import dummy_data from '../reducers/dummy_data'
+
 
 export function fetchNews() {
-  axios.get(process.env.REACT_APP_API_URL, {
-    params: {
-      country: process.env.REACT_APP_API_COUNTRY,
-      category: process.env.REACT_APP_API_CATEGORY,
-      apiKey: process.env.REACT_APP_API_KEY
-    }
-  })
-  .then(response => {
-    console.log('response:', response);
-    this.setState({
-      articles: response.data.articles
-    })
-  })
-  .catch(error => {
-    console.log('get error:', error);
-  })
+
+  // for development only
+  const articles = { data: { articles: dummy_data}}
+  return (dispatch) => {
+    dispatch({ type: 'FETCH_NEWS', articles })
+  }
+
+  // uncomment when project is complete:
+
+  // return (dispatch) => {
+  //   dispatch({ type: 'LOADING_NEWS' });
+  //   axios.get(process.env.REACT_APP_API_URL, {
+  //     params: {
+  //       country: process.env.REACT_APP_API_COUNTRY,
+  //       category: process.env.REACT_APP_API_CATEGORY,
+  //       apiKey: process.env.REACT_APP_API_KEY
+  //     }
+  //   })
+  //   .then(articles => dispatch({ type: 'FETCH_NEWS', articles }));
+  // }
 }
 
 export function fetchSearch(query) {
