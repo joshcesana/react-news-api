77097845068d25ed1a7cc6e54b751d21ab453c37
JCesana <joshuacesana@gmail.com>
2018-12-31 22:32:21 -0800

    Add dummy data for development to avoid API calls

--- src/actions/articlesActions.js
+++ src/actions/articlesActions.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
-import dummy_data from '../reducers/dummy_data'
+import dummyData from '../reducers/dummyData'
 
 
 export function fetchNews() {
 
   // for development only
-  const articles = { data: { articles: dummy_data}}
+  const articles = { data: { articles: dummyData.articles }}
   return (dispatch) => {
     dispatch({ type: 'FETCH_NEWS', articles })
   }
@@ -26,16 +26,26 @@ export function fetchNews() {
 }
 
 export function fetchSearch(query) {
+
+  // for development only
+  const articles = { data: { articles: dummyData.dummySearch }};
+  const searchQuery = query;
   return (dispatch) => {
-    dispatch({ type: 'LOADING_SEARCH' });
-    axios.get(process.env.REACT_APP_API_URL, {
-      params: {
-        country: process.env.REACT_APP_API_COUNTRY,
-        category: process.env.REACT_APP_API_CATEGORY,
-        q: query,
-        apiKey: process.env.REACT_APP_API_KEY
-      }
-    })
-    .then(articles => dispatch({ type: 'FETCH_SEARCH', articles }));
+    dispatch({ type: 'FETCH_SEARCH', articles, searchQuery })
   }
+
+  // uncomment when project complete:
+
+  // return (dispatch) => {
+  //   dispatch({ type: 'LOADING_SEARCH' });
+  //   axios.get(process.env.REACT_APP_API_URL, {
+  //     params: {
+  //       country: process.env.REACT_APP_API_COUNTRY,
+  //       category: process.env.REACT_APP_API_CATEGORY,
+  //       q: query,
+  //       apiKey: process.env.REACT_APP_API_KEY
+  //     }
+  //   })
+  //   .then(articles => dispatch({ type: 'FETCH_SEARCH', articles: articles, searchQuery: query }));
+  // }
 }
