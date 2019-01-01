3e7e537eded04dc7e3f309d077e96fa3bfe2b39d
JCesana <joshuacesana@gmail.com>
2018-12-31 20:29:46 -0800

    Moved file to newly created /components/search

---
+++ src/components/search/SearchForm.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+
+class SearchForm extends Component {
+
+  state = {
+    search: ''
+  }
+
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
+  handleOnSubmit = (event) => {
+    event.preventDefault();
+    this.setState({
+      search: ''
+    })
+    this.props.fetchSearch(this.state.search);
+  }
+
+  render() {
+    return (
+      <div className="row">
+        <form className="col s12" onSubmit={ (event) => this.handleOnSubmit(event) }>
+          <div className="row">
+            <div className="input-field col s6 push-s3 l4 push-l4">
+              <input id="search"
+                     name="search"
+                     value={this.state.search}
+                     placeholder="Search"
+                     type="text"
+                     className="validate"
+                     onChange={this.handleChange}
+              />
+            </div>
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
+
+export default SearchForm;
