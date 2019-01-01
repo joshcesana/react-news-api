7413c0350fce866ec3a20b2d328be2a2611b52c4
JCesana <joshuacesana@gmail.com>
2018-12-31 16:09:15 -0800

    Created search form

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,13 +1,36 @@
 import React, { Component } from 'react';
 
 class SearchForm extends Component {
-  render() {
-    return (
-      <p>SearchForm</p>
-    )
+
+  state = {
+    search: ''
   }
 
+  handleChange = (event) => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
 
+  render() {
+    return (
+      <div className="row">
+        <form className="col s12">
+          <div className="row">
+            <div className="input-field col s6 push-s3 l4 push-l4">
+              <input id="search"
+                     name="search"
+                     value={this.state.search}
+                     placeholder="Search"
+                     type="text"
+                     className="validate"
+                     onChange={this.handleChange} />
+            </div>
+          </div>
+        </form>
+      </div>
+    )
+  }
 }
 
 export default SearchForm;
