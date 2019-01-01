b2a8c3aec9c4adfe52853655356c5504dff0ff37
JCesana <joshuacesana@gmail.com>
2018-12-31 16:40:48 -0800

    Lift away fetch to action

--- src/containers/SearchContainer.js
+++ src/containers/SearchContainer.js
@@ -1,29 +1,11 @@
 import React, { Component } from 'react';
 import SearchForm from '../components/SearchForm';
-import axios from 'axios';
 import { connect } from 'react-redux';
 
 
 class SearchContainer extends Component {
-
   state = {
-    articles: []
-  }
-
-  componentDidMount() {
-    // axios.get(process.env.REACT_APP_API_URL, {
-    //   params: {
-    //     country: process.env.REACT_APP_API_COUNTRY,
-    //     category: process.env.REACT_APP_API_CATEGORY,
-    //     apiKey: process.env.REACT_APP_API_KEY
-    //   }
-    // })
-    // .then(response => {
-    //   console.log('response:', response);
-    // })
-    // .catch(error => {
-    //   console.log('API error:', error);
-    // })
+    search: ''
   }
 
   render() {
