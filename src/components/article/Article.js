92c288a6b286f2fa4d55a7f3ba91ce8a98a5e1f4
JCesana <joshuacesana@gmail.com>
2019-01-02 14:58:54 -0800

    Update component names & import

--- src/components/article/Article.js
+++ src/components/article/Article.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import dateformat from 'dateformat';
-import CardImage from './CardImage';
-import CardContent from './CardContent';
+import ArticleImage from './ArticleImage';
+import ArticleContent from './ArticleContent';
 
 const Article = props => {
   if (props.article) {
     return (
       <div className="card hoverable">
-        <CardImage article={props.article} addSave={props.addSave} />
-        <CardContent article={props.article} />
+        <ArticleImage article={props.article} addSave={props.addSave} />
+        <ArticleContent article={props.article} />
       </div>
     )
   } else {
