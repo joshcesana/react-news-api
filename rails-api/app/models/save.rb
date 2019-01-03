246438e6f42f38e5636321e26e0aea7b6cf8f73a
JCesana <joshuacesana@gmail.com>
2019-01-02 16:45:18 -0800

    Remove validations for description & urlToImage

--- rails-api/app/models/save.rb
+++ rails-api/app/models/save.rb
@@ -1,9 +1,7 @@
 class Save < ApplicationRecord
   validates :source,
             :title,
-            :description,
             :url,
-            :urlToImage,
             :publishedAt,
             presence: true
 end
