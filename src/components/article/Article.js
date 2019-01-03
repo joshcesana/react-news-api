0e952260e61b253b84c47d6aca6299a829aeea5f
JCesana <joshuacesana@gmail.com>
2019-01-02 16:49:13 -0800

    Add handleOnClick, props

--- src/components/article/Article.js
+++ src/components/article/Article.js
@@ -7,7 +7,7 @@ const Article = props => {
   if (props.article) {
     return (
       <div className="card hoverable">
-        <ArticleImage article={props.article} addSave={props.addSave} />
+        <ArticleImage article={props.article} addSave={props.addSave} deleteSave={props.deleteSave} isSaved={props.isSaved} />
         <ArticleContent article={props.article} />
       </div>
     )
