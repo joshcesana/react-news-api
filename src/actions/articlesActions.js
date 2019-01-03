c66b46712d5827bc991f378035ce1b1fda7e166c
JCesana <joshuacesana@gmail.com>
2019-01-02 17:23:32 -0800

    Comment out dummy data, uncomment API calls

--- src/actions/articlesActions.js
+++ src/actions/articlesActions.js
@@ -5,49 +5,47 @@ import dummyData from '../reducers/dummyData'
 export function fetchNews() {
 
   // for development only
-  const articles = { data: { articles: dummyData.articles }}
-  return (dispatch) => {
-    dispatch({ type: 'FETCH_NEWS', articles })
-  }
+  // const articles = { data: { articles: dummyData.articles }}
+  // return (dispatch) => {
+  //   dispatch({ type: 'FETCH_NEWS', articles })
+  // }
 
   // uncomment when project is complete:
-
-  // return (dispatch) => {
-  //   dispatch({ type: 'LOADING_NEWS' });
-  //   axios.get(process.env.REACT_APP_API_URL, {
-  //     params: {
-  //       country: process.env.REACT_APP_API_COUNTRY,
-  //       category: process.env.REACT_APP_API_CATEGORY,
-  //       apiKey: process.env.REACT_APP_API_KEY
-  //     }
-  //   })
-  //   .then(articles => dispatch({ type: 'FETCH_NEWS', articles }));
-  // }
+  return (dispatch) => {
+    dispatch({ type: 'LOADING_NEWS' });
+    axios.get(process.env.REACT_APP_API_URL, {
+      params: {
+        country: process.env.REACT_APP_API_COUNTRY,
+        category: process.env.REACT_APP_API_CATEGORY,
+        apiKey: process.env.REACT_APP_API_KEY
+      }
+    })
+    .then(articles => dispatch({ type: 'FETCH_NEWS', articles }));
+  }
 }
 
 export function fetchSearch(query) {
 
   // for development only
-  const articles = { data: { articles: dummyData.dummySearch }};
-  const searchQuery = query;
-  return (dispatch) => {
-    dispatch({ type: 'FETCH_SEARCH', articles, searchQuery })
-  }
+  // const articles = { data: { articles: dummyData.dummySearch }};
+  // const searchQuery = query;
+  // return (dispatch) => {
+  //   dispatch({ type: 'FETCH_SEARCH', articles, searchQuery })
+  // }
 
   // uncomment when project complete:
-
-  // return (dispatch) => {
-  //   dispatch({ type: 'LOADING_SEARCH' });
-  //   axios.get(process.env.REACT_APP_API_URL, {
-  //     params: {
-  //       country: process.env.REACT_APP_API_COUNTRY,
-  //       category: process.env.REACT_APP_API_CATEGORY,
-  //       q: query,
-  //       apiKey: process.env.REACT_APP_API_KEY
-  //     }
-  //   })
-  //   .then(articles => dispatch({ type: 'FETCH_SEARCH', articles: articles, searchQuery: query }));
-  // }
+  return (dispatch) => {
+    dispatch({ type: 'LOADING_SEARCH' });
+    axios.get(process.env.REACT_APP_API_URL, {
+      params: {
+        country: process.env.REACT_APP_API_COUNTRY,
+        category: process.env.REACT_APP_API_CATEGORY,
+        q: query,
+        apiKey: process.env.REACT_APP_API_KEY
+      }
+    })
+    .then(articles => dispatch({ type: 'FETCH_SEARCH', articles: articles, searchQuery: query }));
+  }
 }
 
 export function fetchSaves() {
