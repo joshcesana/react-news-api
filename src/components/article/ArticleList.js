5de8d2c7a991084e6aea71985ffee3c9d6903af1
JCesana <joshuacesana@gmail.com>
2019-01-02 17:24:19 -0800

    No change

--- src/components/article/ArticleList.js
+++ src/components/article/ArticleList.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ArticleRow from './ArticleRow';
 
 const ArticleList = (props) => {
-  
+
   const rows = props.articles.reduce((resultArray, item, index) => {
     const chunkIndex = Math.floor(index/3) // 3 articles per row
 
